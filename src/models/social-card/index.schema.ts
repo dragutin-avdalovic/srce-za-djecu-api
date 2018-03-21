6bcb68aeeb02e942131312f198d3cc075f566d5b
vildantursic <vildan.tursic@walter.ba>
2018-03-21 10:36:01 +0100

    updated schema

--- src/models/social-card/index.schema.ts
+++ src/models/social-card/index.schema.ts
@@ -45,25 +45,32 @@ const familyMemberSchema = new mongoose.Schema({
 });
 
 const familySchema = new mongoose.Schema({
-  unmarried: Boolean,
-  married: Boolean,
-  divorced: Boolean,
-  widow: Boolean,
-  other: Boolean,
+  meritalStatus: {
+    type: Number,
+    enum: [0, 1, 2, 3, 4, 5]
+  },
   familyMembers: [familyMemberSchema],
   chronicalDecease: Boolean,
+  chronicalDeceaseText: String,
   disability: Boolean,
+  disabilityText: String,
   specialNeeds: Boolean,
   familyRelations: {
     type: Number,
     enum: [0, 1, 2]
   },
   incomeBySalary: Boolean,
+  incomeBySalaryText: String,
   familyPension: Boolean,
+  familyPensionText: String,
   unemploymentBenefit: Boolean,
+  unemploymentBenefitText: String,
   disabilityCompensation: Boolean,
+  disabilityCompensationText: String,
   compensationForTheSocialProtectionSystem: Boolean,
+  compensationForTheSocialProtectionSystemText: String,
   otherIncome: Boolean,
+  otherIncomeText: String,
   familyResidence: {
     type: Number,
     enum: [0, 1, 2]
