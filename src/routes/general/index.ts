ea08e2daa970179f8d483025b2d455c00e76ce22
dragutin-avdalovic <dragutin.avdalovic@walter.ba>
2018-07-05 13:11:57 +0200

    The prototype for import function created...

--- src/routes/general/index.ts
+++ src/routes/general/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { Request, Response } from 'express';
 const json2xls = require('json2xls');
+const xlsxj = require('xlsx-to-json');
 // const jsPDF = require('jspdf');
 // import * as JSPdf from "jspdf";
 import fs from 'fs';
@@ -69,10 +70,54 @@ router.get('/download/:segment/:type', async (req: Request, res: Response) => {
 
 });
 
-// generatePdf = () => {
-//   let doc = new JSPdf();
-//   doc.text("Hello", 20, 20);
-//   doc.save('table.pdf');
-// }
+router.get('/import', (req: Request, res: Response) => {
+  let output: any;
+  let outputOne: any;
+  const table = '/donations';
+  // let content: any;
+  //
+  // fs.readFile(__dirname + '/simple.txt', 'UTF-8', function read(err, data) {
+  //   if (err) {
+  //     throw err;
+  //   }
+  //   content = data;
+  //
+  //   // Invoke the next step here however you like
+  //   console.log(content);   // Put all of the code here (not the best solution)
+  //   processFile();          // Or put the next step in a function and invoke it
+  // });
+  //
+  // function processFile() {
+  //   console.log(content);
+  // }
+
+  xlsxj({
+    input: `${__dirname}/test.xlsx`,
+    output: 'test.json',
+    sheet: 'umain'
+  }, function (err: Error, result: any) {
+    if (err) {
+      console.error(err);
+    } else {
+      //  res.json(result);
+      output = result;
+      outputOne = output['0'];
+      // res.json(output['0']);
+      const donation = new Donation(outputOne);
+      // res.json(donation);
+      donation.save(function (err) {
+        if (err) {
+          console.error(err);
+          res.json(error);
+        } else {
+          res.json('saved');
+        }
+      }).then(function(users) {
+        return users
+      });
+
+    }
+  });
+});
 
 module.exports = router;
